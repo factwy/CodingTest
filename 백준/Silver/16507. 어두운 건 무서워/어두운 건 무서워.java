f0880ed1a52978492989d5e83c5083dbb5769113
factwy <52695468+factwy@users.noreply.github.com>
2024-03-15 18:49:59 +0900

    [Silver I] Title: 어두운 건 무서워, Time: 660 ms, Memory: 70388 KB -BaekjoonHub

---
+++ 백준/Silver/16507. 어두운 건 무서워/어두운 건 무서워.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int q = Integer.parseInt(st.nextToken());
+		
+		int[][] prefix = new int[r+1][c+1];
+		for(int i=0; i<r+1; i++) {
+			if(i != 0)
+				st = new StringTokenizer(br.readLine());
+			for(int j=0; j<c+1; j++) {
+				if(i==0 || j==0)
+					prefix[i][j] = 0;
+				else {
+					int num = Integer.parseInt(st.nextToken());
+					prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + num;
+				}
+			}
+		}
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		for(int i=0; i<q; i++) {
+			st = new StringTokenizer(br.readLine());
+			int r1 = Integer.parseInt(st.nextToken());
+			int c1 = Integer.parseInt(st.nextToken());
+			int r2 = Integer.parseInt(st.nextToken());
+			int c2 = Integer.parseInt(st.nextToken());
+			
+			int sum = prefix[r2][c2] - prefix[r2][c1-1] - prefix[r1-1][c2] + prefix[r1-1][c1-1];
+			int res = sum / ((r2 - r1 + 1) * (c2 - c1 + 1));
+			bw.write(res + "\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
