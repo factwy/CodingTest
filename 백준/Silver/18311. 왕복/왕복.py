d0f5e601e4c969f486bfb2e0246ef03127df84a4
factwy <52695468+factwy@users.noreply.github.com>
2024-04-08 11:26:16 +0900

    [Silver V] Title: 왕복, Time: 136 ms, Memory: 121008 KB -BaekjoonHub

--- 백준/Silver/18311. 왕복/왕복.py
+++ 백준/Silver/18311. 왕복/왕복.py
@@ -3,18 +3,13 @@ import sys
 n, k = map(int, sys.stdin.readline().split())
 input_data = list(map(int, sys.stdin.readline().split()))
 
-dist = []
-start = 0
-for d in input_data:
-    dist.append([start, start+d])
-    start += d
-
 total_size = sum(input_data)
 if k > total_size:
     k = 2 * total_size - k
 
-for i in range(n):
-    if dist[i][0] <= k < dist[i][1]:
+start = 0
+for i, d in enumerate(input_data):
+    if start <= k < start + d:
         sys.stdout.write(str(i+1))
         break
-        
+    start += d
