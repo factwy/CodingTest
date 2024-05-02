9d23f9474b4fa16da24da7e565576be3471fb7a1
factwy <52695468+factwy@users.noreply.github.com>
2024-05-02 19:52:36 +0900

    [Silver III] Title: 블로그, Time: 164 ms, Memory: 148824 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.py
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -1,23 +1,24 @@
 import sys
 
 n, x = map(int, sys.stdin.readline().split())
-visit = list(map(int, sys.stdin.readline().split()))
-visit = [0] + visit
+arr = list(map(int, sys.stdin.readline().split()))
+prefix = [0 for _ in range(n+1)]
 
 for i in range(1, n+1):
-    visit[i] += visit[i-1]
+    prefix[i] = prefix[i-1] + arr[i-1]
 
 max_visited, cnt = 0, 0
-
 for i in range(n+1-x):
-    visit_now = visit[i+x] - visit[i]
-    if visit_now > max_visited:
-        max_visited = visit_now
+    visited = prefix[i+x] - prefix[i]
+
+    if visited > max_visited:
+        max_visited = visited
         cnt = 1
-    elif visit_now == max_visited:
+    elif visited == max_visited:
         cnt += 1
 
-if max_visited == 0:
-    sys.stdout.write("SAD")
+if max_visited > 0:
+    sys.stdout.write(f"{max_visited}\n{cnt}\n")
 else:
-    sys.stdout.write(str(max_visited) + '\n' + str(cnt))
+    sys.stdout.write("SAD\n")
+    
